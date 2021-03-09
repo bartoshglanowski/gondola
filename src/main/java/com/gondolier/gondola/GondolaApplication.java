3cb088e1f1ac1e5b8c2032d6f34176d8cb0b5d92
coolprit <sk-jr@hotmail.com>
2021-03-09 20:17:14 +0100

    repo initialization

---
+++ src/main/java/com/gondolier/gondola/GondolaApplication.java
@@ -0,0 +1,13 @@
+package com.gondolier.gondola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GondolaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GondolaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gondolier/gondola/GondolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gondolier.gondola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GondolaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
